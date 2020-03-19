3b072749c316d91c0966756f98be3640a6680eaf
CSITEd Experts <CSITEdExperts@gmail.com>
2020-03-19 11:27:39 -0400

    How to make a Web crawler using Java webpage added

--- src/encryptions/EncAlgorithms.java
+++ src/encryptions/EncAlgorithms.java
@@ -30,4 +30,39 @@ public class EncAlgorithms{
 		return diffModEncNumber;
 
 	}
+
+
+	public static String diffModDec(String phNumber) {
+
+		phNumber = diffModEnc(phNumber);
+		
+		int[] diffArray = new int[100];
+		int sumDiff = 0;
+		
+		System.out.print("Digits differences are: ");
+		
+		for (int i = 0; i< phNumber.length()-1; i++)
+		{
+
+			diffArray[i] = phNumber.charAt(i+1) - phNumber.charAt(i);
+			System.out.print(diffArray[i] + " ");
+			sumDiff += diffArray[i]; 
+		}
+
+		System.out.println("\nSum of the absolute digit differences is: " + Math.abs(sumDiff));
+		int subTo = Math.abs(sumDiff);
+		
+		String diffModDecNumber = "";
+		for (int i = 0; i< phNumber.length(); i++)
+		{
+			if ((Character.getNumericValue(phNumber.charAt(i)) + subTo) > 10) {
+			diffModDecNumber += ((Character.getNumericValue(phNumber.charAt(i)) + subTo) % 10);
+			}
+			else 
+				diffModDecNumber += (((Character.getNumericValue(phNumber.charAt(i)) - subTo)+10) %10);
+				
+		}
+		return diffModDecNumber;
+
+	}
 }
--- src/encryptions/EncryptionAlgMain.java
+++ src/encryptions/EncryptionAlgMain.java
@@ -12,13 +12,14 @@ public class EncryptionAlgMain {
 		while (!number.equals(flag)){
 			System.out.print("Enter a phone number [000 to exit]: ");
 			number = scanner.nextLine();
-			if (number.equals(flag)) 
-			{
+			if (number.equals(flag)) {
 				System.err.println("Program terminnated");
 				return;
 			} 
 			else{
 				System.out.println("Using diffModEnc(), the encrypted phone number is: " + EncAlgorithms.diffModEnc(number));
+		
+				System.out.println("Using diffModDec(), the decrypted phone number is: " + EncAlgorithms.diffModDec(number));
 				
 			}
 
